a7549414e722d2202ba1489fca3c506a48e5c5e1
Aadyant Maity <aadyantmaity@users.noreply.github.com>
2025-02-10 12:54:13 -0800

    right sidebar/logout ui

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -2,7 +2,18 @@ import React from "react";
 import "./ProfileUpdate.css";
 
 const ProfileUpdate = () => {
-  return <div>We are on the profile page</div>;
+  return (
+    <div className="profile">
+      <div className="profile-container">
+        <form>
+          <h3>Profile Details</h3>
+          <label htmlFor="avatar">
+            <input type="file" id="avatar" />
+          </label>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default ProfileUpdate;
