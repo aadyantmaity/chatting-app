0a360e8a28f43459253d19c3fa8ad63ff4706d04
Aadyant Maity <aadyantmaity@users.noreply.github.com>
2025-02-10 16:17:02 -0800

    signup func.

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore, setDoc } from "firebase/firestore";
+import { toast } from "react-toastify";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBHDK_FHWAULFqeO2a6kcBjCpNfRkkeXD4",
@@ -33,5 +34,8 @@ const signup = async (username, email, password) => {
     });
   } catch (error) {
     console.error(error);
+    toast.error(error.code);
   }
 };
+
+export { signup };
